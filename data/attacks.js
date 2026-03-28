91742a3c712aa35114b1c33507e7c9c665048c2c
devil27062007 <devil27062007@gmail.com>
2026-03-28 16:31:19 +0530

    music added and villagers added and battle to towwn town to battle field connection added

--- data/attacks.js
+++ data/attacks.js
@@ -1,12 +1,14 @@
-const attacks = { 
-    Tackle : {
-        name : 'Tackle' ,
-        damage : 10 ,
-        type : 'Normal'
-    },
-    Fireball : {
-        name : 'Fireball' ,
-        damage : 25 ,
-        type : 'Fire'
-    }
+const attacks = {
+  Tackle: {
+    name: 'Tackle',
+    damage: 10,
+    type: 'Normal',
+    color: 'black'
+  },
+  Fireball: {
+    name: 'Fireball',
+    damage: 25,
+    type: 'Fire',
+    color: 'red'
+  }
 }
--- data/monsters.js
+++ data/monsters.js
@@ -1,37 +1,35 @@
-const embyImage = new Image()
-embyImage.src = './images/embySprite.png'
-
-const draggleImage = new Image()
-draggleImage.src = './images/draggleSprite.png'
-
 const monsters = {
-    Emby: {
-        position: {
-            x: 280,
-            y: 325,
-        },
-        image: embyImage,
-        frames: {
-            max: 4,
-            hold: 30
-        },
-        animate: true,
-        name: 'Emby',
-        attacks: [attacks.Tackle, attacks.Fireball]
+  Emby: {
+    position: {
+      x: 280,
+      y: 325
     },
-    Draggle: {
-        position: {
-            x: 800,
-            y: 100
-        },
-        image: draggleImage,
-        frames: {
-            max: 4,
-            hold: 30
-        },
-        animate: true,
-        isEnemy: true,
-        name: 'Draggle',
-        attacks: [attacks.Tackle, attacks.Fireball]
-    }
+    image: {
+      src: './img/embySprite.png'
+    },
+    frames: {
+      max: 4,
+      hold: 30
+    },
+    animate: true,
+    name: 'Emby',
+    attacks: [attacks.Tackle, attacks.Fireball]
+  },
+  Draggle: {
+    position: {
+      x: 800,
+      y: 100
+    },
+    image: {
+      src: './img/draggleSprite.png'
+    },
+    frames: {
+      max: 4,
+      hold: 30
+    },
+    animate: true,
+    isEnemy: true,
+    name: 'Draggle',
+    attacks: [attacks.Tackle, attacks.Fireball]
+  }
 }
